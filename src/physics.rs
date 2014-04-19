53d5e0a13eb3c761f7f8fb63a57997d4f1132775
Sven Nilsen <bvssvni@gmail.com>
2014-04-19 17:45:42 +0200

    Added WithAcceleration

--- src/physics.rs
+++ src/physics.rs
@@ -8,6 +8,7 @@ pub struct DeltaTime(f64);
 
 pub type Position = [f64, ..3];
 pub type Velocity = [f64, ..3];
+pub type Acceleration = [f64, ..3];
 
 /// Object in motion under no force.
 pub struct NoForce {
@@ -26,4 +27,23 @@ impl UpdateDelta<DeltaTime> for NoForce {
     }
 }
 
+/// Moves point with acceleration.
+pub struct WithAcceleration {
+    /// The position of object.
+    pub pos: Position,
+    /// The velocity of object.
+    pub vel: Velocity,
+    /// The acceleration on object.
+    pub acc: Acceleration,
+}
+
+impl UpdateDelta<DeltaTime> for WithAcceleration {
+    #[inline(always)]
+    fn update(&mut self, &DeltaTime(dt): &DeltaTime) {
+        for i in range(0u, 3) {
+            let vel_next = self.vel[i] + self.acc[i] * dt;
+            self.pos[i] += 0.5 * dt * (self.vel[i] + vel_next);
+        }
+    }
+}
 
